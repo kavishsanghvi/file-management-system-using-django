17dec628f6a19eb107938343c15b45dc23d2efb4
Kavish Sanghvi <sanghvi_kavish@yahoo.in>
2020-11-22 11:55:59 +0530

    Add files via upload

---
+++ blog/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+import os
+
+class Post(models.Model):
+	title = models.CharField(max_length=100)
+	file = models.FileField(null=True,blank=True,upload_to='Files')
+	content = models.TextField()
+	date_posted = models.DateTimeField(default=timezone.now)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.title
+
+	def extension(self):
+		name, extension = os.path.splitext(self.file.name)
+		return extension
+
+	def get_absolute_url(self):
+		return reverse('post-detail', kwargs={'pk': self.pk})
+
+        
